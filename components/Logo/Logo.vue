d48816a8a1927d6ae7de6af936714d2c3308f05f
xuan <13587013852@163.com>
2022-06-09 17:28:47 +0800

    Logo

---
+++ components/Logo/Logo.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="logo-container">
+    <div class="logo" :class="type">
+      <img :src="url" />
+    </div>
+    <div class="logo-title" :class="type">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 图片是否需要填满整个容器
+    type: {
+      type: String,
+      default: "fill",
+    },
+    // 图片的链接地址
+    url: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.logo-container {
+  width: inherit;
+  height: 60px;
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 9px;
+  font-size: 14px;
+  overflow: hidden;
+  .logo {
+    &.fill {
+      width: 100%;
+      height: 100%;
+    }
+    &.nofill {
+      width: 45px;
+      height: 45px;
+      min-width: 45px;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .logo-title {
+    &.fill {
+      display: none;
+    }
+    margin-left: 12px;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ components/Logo/README.md
@@ -0,0 +1,19 @@
+## 组件库
+
+### Logo
+
+示例：
+| |效果|
+|-|-|
+|type === 'fill'  | ![logo-nofill](./assets/Logo/logo-fill.png)|
+|type === 'nofill'| ![logo-nofill](./assets/Logo/logo-nofill.png)|
+
+> 当 `type === 'fill'` 时，`title` 就会隐藏，图片会覆盖满整个容器
+
+参数
+|参数名|函数|是否必填|默认值|值选项|值类型|
+|-|-|-|-|-|-|
+|url|logo 的图片地址|true|-|-|Boolean|
+|title|要显示的文字|false|-|-|String|
+|type|是否要填满整个容器|false|fill|'fill'、'nofill'|String|
+
